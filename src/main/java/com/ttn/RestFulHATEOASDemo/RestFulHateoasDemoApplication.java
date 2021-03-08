3d27002c8944e30e91641ab2813ee559c4933084
vardanttn <vardan.balyan@tothenew.com>
2021-03-08 22:40:16 +0530

    done internationalization

--- src/main/java/com/ttn/RestFulHATEOASDemo/RestFulHateoasDemoApplication.java
+++ src/main/java/com/ttn/RestFulHATEOASDemo/RestFulHateoasDemoApplication.java
@@ -2,6 +2,14 @@ package com.ttn.RestFulHATEOASDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @SpringBootApplication
 public class RestFulHateoasDemoApplication {
@@ -10,4 +18,19 @@ public class RestFulHateoasDemoApplication {
 		SpringApplication.run(RestFulHateoasDemoApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver()
+	{
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.UK);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource()
+	{
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
--- src/main/java/com/ttn/RestFulHATEOASDemo/user/GoodMorningMessage.java
+++ src/main/java/com/ttn/RestFulHATEOASDemo/user/GoodMorningMessage.java
@@ -1,14 +1,29 @@
 package com.ttn.RestFulHATEOASDemo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class GoodMorningMessage {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/good-morning")
     public String print()
     {
         return "Good Morning!!";
+    }
+
+    @GetMapping(path = "/good-morning-international")
+    public String goodMorningInternationalization(@RequestHeader(name = "Accept-Language",
+            required = false) Locale locale)
+    {
+        return messageSource.getMessage("good.morning.message",null,locale);
     }
 }
