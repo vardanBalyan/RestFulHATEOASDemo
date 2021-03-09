334ff1974ade1fbc595d371c45bd38fb8103dff2
vardanttn <vardan.balyan@tothenew.com>
2021-03-09 22:03:18 +0530

    added swagger

--- src/main/java/com/ttn/RestFulHATEOASDemo/user/User.java
+++ src/main/java/com/ttn/RestFulHATEOASDemo/user/User.java
@@ -1,9 +1,21 @@
 package com.ttn.RestFulHATEOASDemo.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+@ApiModel(description = "user class contains all the structure of the user")
 public class User {
 
     private int id;
+    @Size(min = 3)
+    @ApiModelProperty(notes = "name should contains atleast 3 characters")
     private String name;
+    @Range(min = 10, max = 105)
+    @ApiModelProperty(notes = "age should be within the range of 10 to 105")
     private int age;
 
     public User(int id, String name, int age) {
--- src/main/java/com/ttn/RestFulHATEOASDemo/user/UserResource.java
+++ src/main/java/com/ttn/RestFulHATEOASDemo/user/UserResource.java
@@ -1,27 +1,33 @@
 package com.ttn.RestFulHATEOASDemo.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+@ApiModel(description = "user resource class")
 @RestController
 public class UserResource {
 
     @Autowired
     private UserDaoService service;
 
+    @ApiModelProperty(notes = "returns all the users from the list of users")
     @GetMapping(path = "/user")
     public List<User> getAllUser()
     {
         return service.fetchAllUsers();
     }
 
+    @ApiModelProperty(notes = "adds new user to the list of user")
     @PostMapping(path = "/user")
-    public ResponseEntity<Object> createUser(@RequestBody User user)
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user)
     {
         User addedUser =  service.saveUser(user);
         URI location = ServletUriComponentsBuilder
@@ -33,6 +39,7 @@ public class UserResource {
 
     }
 
+    @ApiModelProperty(notes = "deletes a user for the provided specific id")
     @DeleteMapping(path = "/user/{id}")
     public void deleteUser(@PathVariable int id)
     {
